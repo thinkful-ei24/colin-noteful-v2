b14c42ac5a6a2a9e73b87d75e64c0f7b5747291a
Colin Rupp <colin@thinkful.com>
2018-09-12 13:21:32 -0700

    added endpoints to routes/folders.js

--- routes/folders.js
+++ routes/folders.js
@@ -26,5 +26,55 @@ router.get('/:id', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.put('/:id', (req, res, next) => {
+  //make variables to acputre the id and object that will be updated 
+  const id = req.params.id;
+  const updateObj = {};
+  updateObj.name = req.body.name;
+  if (!updateObj.name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  };
+  
+  knex('folders')
+    .where({id: `${id}`})
+    .update(updateObj)
+    .returning(['id', 'name'])
+    .then(reply => {
+      res.status(201).json(reply[0])
+    })
+    .catch (err => next(err));
+});
+
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+  const newItem = { name };
+  newItem.name = req.body.name
+  if (!newItem.name) {
+    const err = new Error('New folder must contain a `name`');
+    err.status = 400;
+    return next(err);
+  }
+  knex('folders')
+    .insert(newItem)
+    .returning(['name', 'id'])
+    .then (reply => {
+      res.status(201).json(reply)
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  knex('folders')
+    .where({id: `${id}`})
+    .del()
+    .then(() => res.sendStatus(204))
+    .catch(err => next(err))
+});
 
 module.exports = router;
+// notes on destructuring objects
+//const name = req.body.name // ='foo'
+//const newItem = { name } // { name: 'foo' }
